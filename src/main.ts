1744ce0462399d55225ed148a8e7fca6b3cfcee1
Ruchika Jain <31808822+ruchika-jain@users.noreply.github.com>
2022-06-01 19:57:12 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import {wait} from './wait'
+import * as docker from './docker';
+import * as oras from './oras';
 
 async function run(): Promise<void> {
   try {
@@ -13,9 +15,24 @@ async function run(): Promise<void> {
 
     core.setOutput('time', new Date().toTimeString())
     
-    const TOKEN = core.getInput('token');
-    
+    const TOKEN = core.getInput('token'); 
     core.setSecret(TOKEN);
+    const repo_owner_and_name: string[2] = core.getInput('repository')
+    const repository_owner: string = core.getInput('repository_owner')
+    
+    const standalone = !(await oras.isAvailable());
+
+    if (standalone) {
+      core.info(`Oras info skipped in standalone mode`);
+    } else {
+      await exec.exec('oras', ['version'], {
+        failOnStdErr: false
+      });
+    }
+    if (!(await buildx.isAvailable(standalone))) {
+      core.setFailed(`Docker buildx is required. See https://github.com/docker/setup-buildx-action to set up buildx.`);
+      return;
+    }
     
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
